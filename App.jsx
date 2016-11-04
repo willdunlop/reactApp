d29bd3f063906b45a76a2fe87fd6bec5fcd25a69
willdunlop <william.dunlop.1993@gmail.com>
2016-11-04 14:32:00 +1100

    Basic Page has been set up

--- App.jsx
+++ App.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 class App extends React.Component {
 	render() {
+
+		var someStyle = {
+			border: solid, thick,
+			width: 200,
+			height: auto,
+		}
+
 		return (
-				<div>Hello World!!</div>
+				<div>
+					<h1>The Heading</h1>
+					<h2>The Content</h2>
+					<p>This is content that comes after headings</p>
+					<p> JSX allows you to use javascript logic when it is wrapped in curly braces. For example 1 + 1 = {1+1}</p>
+					<p>Although you cant us if/else statements, ternary/conditional expressions are available {i == 1 ? 'true' : 'false'}</p>
+
+					<p style = {someStyle}>Styling is achieved by setting a variable before the return() section under render and naming using interpolation in the divs style tag</p>
+
+					{//Comments need to be written with JS interpolation
+					}
+				</div>
 			   );
 	}
 }
